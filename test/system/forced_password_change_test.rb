e30fc85f3a10151bd574c47c9c9914f02c309268
Joonas Kykkänen <jonaskay@iki.fi>
2026-03-24 19:37:52 +0200

    Disable Chrome password manager in system tests
    Chrome's password manager was clearing the password confirmation
    field after the password field was filled, causing the
    ForcedPasswordChangeTest to consistently fail on CI. Disabling
    Chrome's credential service and password manager prevents this
    autofill interference.

--- test/system/forced_password_change_test.rb
+++ test/system/forced_password_change_test.rb
@@ -9,8 +9,8 @@ class ForcedPasswordChangeTest < ApplicationSystemTestCase
 
     assert_current_path edit_account_password_path
 
-    find('input[name="user[password]"]').set("newpassword123!")
-    find('input[name="user[password_confirmation]"]').set("newpassword123!")
+    fill_in I18n.t("account.passwords.edit.password_placeholder"), with: "newpassword123!"
+    fill_in I18n.t("account.passwords.edit.password_confirmation_placeholder"), with: "newpassword123!"
     click_button_and_confirm I18n.t("account.passwords.edit.submit"), title: I18n.t("sessions.new.title")
 
     assert_text I18n.t("account.passwords.update.success")
